d50db4ccd087687c33a01db0785e0b17c81f11e2
Kenny Xiong <xiokenw@dunwoody.edu>
2018-04-18 13:30:11 -0500

    Created Partial View for Product Summary, Created View: RarityMenu, which will filter products, Created RarityController

--- theBlackSmithsHouse.WebUI/Controllers/RarityController.cs
+++ theBlackSmithsHouse.WebUI/Controllers/RarityController.cs
@@ -1,12 +1,26 @@
 ﻿using System.Web.Mvc;
+using System.Collections.Generic;
+using theBlackSmithsHouse.Domain.Abstract;
+using System.Linq;
 
+// This controller is for the Rarity list which user can select to filter the products
 namespace theBlackSmithsHouse.WebUI.Controllers
 {
     public class RarityController : Controller
     {
-        public string RarityMenu()
+        private IProductRepository repository;
+        // add constructor that accepts IProductRepository as argument
+        public RarityController (IProductRepository repo)
         {
-            return "Hello from RarityController";
+            repository = repo;
+        }
+
+        public PartialViewResult RarityMenu()
+        {               // use Linq query to obtain list of rarities from repository to pass to the view
+            IEnumerable<string> rarities = repository.Products.Select(x => x.Rarity)
+                                                              .Distinct()
+                                                              .OrderBy(x => x);
+            return PartialView(rarities);
         }
     }
 }
